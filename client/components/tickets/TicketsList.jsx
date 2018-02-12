e3af09993f428e8dbe8c504a90cc0bef56a71524
The_beast\Samuel Lawrentz <samuellawrentz@gmail.com>
2018-02-12 21:38:05 +0530

    Simplified the card view and added styles

--- client/components/tickets/TicketsList.jsx
+++ client/components/tickets/TicketsList.jsx
@@ -43,31 +43,14 @@ class TicketsList extends React.Component {
                             showExpandableButton={true}
                             
                         />
-                        <CardText expandable={true}>
-                            <table class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
-                                <tbody>
-                                    <tr>
-                                        <td class="mdl-data-table__cell--non-numeric">Reporter</td>
-                                        <td class="mdl-data-table__cell--non-numeric">{ticket.reporter}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="mdl-data-table__cell--non-numeric">Assignee</td>
-                                        <td class="mdl-data-table__cell--non-numeric">{ticket.assignee}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="mdl-data-table__cell--non-numeric">Status</td>
-                                        <td class="mdl-data-table__cell--non-numeric">{ticket.status}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="mdl-data-table__cell--non-numeric">Severity</td>
-                                        <td class="mdl-data-table__cell--non-numeric">{ticket.severity}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        <CardText expandable={true}>                            
+                            <div style={{"display": "flex", "justify-content":"space-between"}}>
+                            <div>{ticket.description}</div>
+                                <div style={{"padding-right":"20px"}}>Asignee: {ticket.assignee}</div>
+                            </div>
                         </CardText>
-                        <CardActions expandable={true}>
-                            <FlatButton label="Edit" />
-                            <FlatButton label="Delete" />
+                        <CardActions expandable={true}>                            
+                        <div><FlatButton label="View details"/></div>
                         </CardActions>
                     </Card>
                 )}
