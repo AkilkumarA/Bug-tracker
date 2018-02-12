f6dc1aa3cbddce11ee8d0ed10ec7a490f42f64ba
Akil kumar <akilkumar96@gmail.com>
2018-02-12 20:37:56 +0530

    Added the UI-Router

---
+++ client/components/board/ProgressBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ProgressBoard extends React.Component {
+
+    render() {
+        return (
+            <div>Board view</div>
+        );
+    }
+}
+
+export default ProgressBoard;
--- client/components/layout/RootView.jsx
+++ client/components/layout/RootView.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Router, Route, Link } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 import Header from './Header';
 import Footer    from './Footer';
-import ViewTickets from '../viewTickets/ViewTickets';
+import TicketsList from '../tickets/TicketsList';
+import Board from '../board/ProgressBoard';
 
 class RootView extends React.Component {
     render() {
+        const history = createBrowserHistory();
         return (
-            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-                <Header />
-                <main className="mdl-layout__content">
-                    <div className="page-content" style={{"padding":"30px"}}>
-                        <ViewTickets />
-                    </div>
-                </main>
-                <Footer />
-            </div>
+            <Router history={history}>
+                <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+                    <Header />
+                    <main className="mdl-layout__content">
+                        <div className="page-content" style={{"padding":"30px"}}>
+                            <Route path="/tickets" component={() => <TicketsList />} />
+                            <Route path="/board" component={() => <Board />} />
+                        </div>
+                    </main>
+                    <Footer />
+                </div>
+            </Router>
         );
     }
 }
