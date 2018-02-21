bdaecba391cbd0ac9f94d833f1249b57f6df1be3
Akil kumar <akilkumar96@gmail.com>
2018-02-21 19:02:32 +0530

    Added a vertical navigation

--- client/components/layout/RootView.jsx
+++ client/components/layout/RootView.jsx
@@ -3,7 +3,7 @@ import { Router, Route, Link } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import Header from './Header';
-import Footer    from './Footer';
+import Footer from './Footer';
 import TicketsList from '../tickets/TicketsList';
 import Board from '../board/ProgressBoard';
 
@@ -14,8 +14,15 @@ class RootView extends React.Component {
             <Router history={history}>
                 <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
                     <Header />
+                    <div class="mdl-layout__drawer">
+                        <span class="mdl-layout-title">Bug Tracker</span>
+                        <nav class="mdl-navigation">
+                            <a class="mdl-navigation__link" href="/tickets">Issues</a>
+                            <a class="mdl-navigation__link" href="/board">Progress Board</a>
+                        </nav>
+                    </div>
                     <main className="mdl-layout__content">
-                        <div className="page-content" style={{"padding":"30px"}}>
+                        <div className="page-content" style={{ "padding": "30px" }}>
                             <Route path="/tickets" component={() => <TicketsList />} />
                             <Route path="/board" component={() => <Board />} />
                         </div>
